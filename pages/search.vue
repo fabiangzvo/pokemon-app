556a02734b5c1e96451510cd9e42ba1a0f0715d5
fabiangzvo <faguzman.97@gmail.com>
2025-02-23 01:30:32 -0500

    implement api search

--- pages/search.vue
+++ pages/search.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="container">
-    <SearchInput
-      v-model="query"
-      @search="router.push({ path: '/search', query: { query, page: 0 } })"
-    />
+    <SearchInput v-model="query" @search="onSubmitSearch" />
   </div>
+  <BottomBar />
 </template>
 
 <script setup>
 const router = useRouter();
 
 const query = ref("");
-</script>
 
-<style lang="scss" scoped></style>
+const { $axios } = useNuxtApp();
+
+async function onSubmitSearch() {
+  const queryParams = { page: 0 };
+
+  if (query.value) queryParams.query = query.value;
+
+  router.push({ path: "/search", query: queryParams });
+}
+
+async function handleSearch() {
+  try {
+    const path = `pokemon${query.value ? "/".concat(query.value) : ""}`;
+    const response = await $axios.get(path);
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error al obtener datos:", error);
+  }
+}
+
+watch(
+  () => [router.currentRoute.value.query],
+  async () => await handleSearch()
+);
+
+onMounted(async () => await handleSearch());
+</script>
