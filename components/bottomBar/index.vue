86b4796d31b1c39f633a3ec7cc7e5964daeedaae
fabiangzvo <faguzman.97@gmail.com>
2025-02-23 01:30:16 -0500

    add bottom bar

---
+++ components/bottomBar/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav
+    class="absolute w-full h-20 bottom-0 flex items-center justify-center gap-x-5"
+  >
+    <Button
+      class="h-11 w-[275px]"
+      label="All"
+      icon="pi pi-list"
+      severity="danger"
+      rounded
+    />
+    <Button
+      class="h-11 w-[275px]"
+      label="Favorites"
+      severity="danger"
+      icon="pi pi-star-fill"
+      rounded
+    />
+  </nav>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
