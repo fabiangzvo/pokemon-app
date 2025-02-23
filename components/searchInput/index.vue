484302d9fd50e694457f5db3f770822c01be59fb
fabiangzvo <faguzman.97@gmail.com>
2025-02-23 01:29:58 -0500

    change search input UI

--- components/searchInput/index.vue
+++ components/searchInput/index.vue
@@ -1,25 +1,13 @@
 <template>
-  <InputGroup class="shadow-md h-14 rounded-xl bg-white dark:bg-black/30">
-    <InputGroupAddon
-      unstyled
-      class="pr-4 h-full flex items-center justify-center"
-    >
-      <Button
-        class="w-full h-full active:!bg-transparent hover:!bg-transparent pl-4 text-gray-500"
-        icon="pi pi-search"
-        severity="primary"
-        variant="text"
-        @click="emit('search')"
-      />
-    </InputGroupAddon>
+  <IconField class="shadow-md mt-9">
+    <InputIcon class="pi pi-search" />
     <InputText
-      unstyled
-      class="w-full pl-6 focus-visible:outline-none"
-      placeholder="Buscar imágenes..."
       v-model="model"
+      class="w-full"
+      placeholder="Search"
       @keypress.enter="emit('search')"
     />
-  </InputGroup>
+  </IconField>
 </template>
 
 <script setup lang="ts">
