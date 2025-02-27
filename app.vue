874f3366fb8738db237acee0dca41d0df24bfff8
fabiangzvo <faguzman.97@gmail.com>
2025-02-27 12:41:02 -0500

    add and configure vue-sonner

--- app.vue
+++ app.vue
@@ -2,8 +2,11 @@
   <main class="relative w-screen h-screen flex justify-center">
     <NuxtRouteAnnouncer />
     <NuxtPage />
+    <Toaster richColors position="top-right" />
   </main>
 </template>
 <script setup lang="ts">
 import "primeicons/primeicons.css";
+
+import { Toaster } from "vue-sonner";
 </script>
